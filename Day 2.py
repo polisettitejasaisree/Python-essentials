af92dc1c7b7f7299604e840e9c52534168c88456
polisettitejasaisree <48091385+polisettitejasaisree@users.noreply.github.com>
2020-07-22 00:58:19 +0530

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,245 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("HI")
+
+
+# In[2]:
+
+
+print("Hi 
+      hello")
+
+
+# In[3]:
+
+
+print("Hi / hello")
+
+
+# In[4]:
+
+
+print("Hi \ hello")
+
+
+# In[5]:
+
+
+print("Hi hello")
+
+
+# In[6]:
+
+
+name="teja"
+marks=92.349
+rollno=12
+print("hi my name is ",name,"my marks",marks,"rollno is",rollno)
+
+
+# In[7]:
+
+
+name="teja"
+marks=92.349
+rollno=12
+print("hi my name is ",name,"my marks",marks,"rollno is",rollno)
+
+
+# In[8]:
+
+
+name="teja"
+marks=92.349
+rollno=12
+print(f"hi my name is {name} my marks {marks} rollno is {rollno})
+
+
+# In[9]:
+
+
+name="teja"
+marks=92.349
+rollno=12
+print(f"hi my name is {name} my marks {marks} rollno is {rollno}")
+
+
+# In[10]:
+
+
+name="teja"
+marks=92.349
+rollno=12
+print(f"hi my name is %s my marks %f rollno is %d)
+
+
+# In[11]:
+
+
+name="teja"
+marks=92.349
+rollno=12
+print(f"hi my name is %s my marks %f rollno is %d")
+
+
+# In[12]:
+
+
+name="teja"
+marks=92.349
+rollno=12
+print(f"hi my name is %s my marks %f rollno is %d"%(name,marks,rollno))
+
+
+# In[13]:
+
+
+name="teja"
+marks=92.349
+rollno=12
+print(f"hi my name is %s my marks %10f rollno is %d"%(name,marks,rollno))
+
+
+# In[14]:
+
+
+name="teja"
+marks=92.349
+rollno=12
+print(f"hi my name is %s my marks %.2f rollno is %d"%(name,marks,rollno))
+
+
+# In[15]:
+
+
+x=10
+id(x)
+
+
+# In[16]:
+
+
+x=10
+id(x)
+y=10
+id(y)
+
+
+# In[18]:
+
+
+del x
+id(x)
+
+
+# In[19]:
+
+
+x=y=10
+id(x)
+
+
+# In[20]:
+
+
+x=y=10
+id(x)
+id(y)
+
+
+# In[21]:
+
+
+x=20
+y=10
+id(x)
+id(y)
+
+
+# In[22]:
+
+
+1/2
+
+
+# In[23]:
+
+
+1//2
+
+
+# In[24]:
+
+
+2**3
+
+
+# In[26]:
+
+
+10>9
+
+
+# In[27]:
+
+
+1&0
+
+
+# In[28]:
+
+
+1&1
+
+
+# In[29]:
+
+
+1|1
+
+
+# In[30]:
+
+
+1|0
+
+
+# In[31]:
+
+
+0|0
+
+
+# In[32]:
+
+
+10>9 and 10<9
+
+
+# In[33]:
+
+
+10>9 or 10<9
+
+
+# In[34]:
+
+
+10>8>9
+
+
+# In[35]:
+
+
+10>8 and 8>9
+
+
+# In[ ]:
+
+
+
+
